9223435a74f44a4eb4c76046f4fdfd6ba01b7b21
Nick Ducker <nick_ducker@live.com>
2022-02-28 23:04:25 +1100

    I love using _ in integers

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let number = 9000;
+    let number = 12_0000;
 
+
+    // must be a bool, can't do that whacky JS stuff
     if number < 9001 {
         println!("This is a boring number")
-    } else {
+    } else if number > 9000 && number < 10_000 {
         println!("IT'S OVER 9000!")
+    } else {
+        println!("WREEEEE")
     }
 }
