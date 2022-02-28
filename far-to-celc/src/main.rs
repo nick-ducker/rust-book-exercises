1d330d05de9f00307faa94f49c2c7e30dcdb3c13
Nick Ducker <nick_ducker@live.com>
2022-02-28 23:50:15 +1100

    create basic celcius to fahrenheigh calculator

--- far-to-celc/src/main.rs
+++ far-to-celc/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let fahrenheit = 104.00;
+
+    let converted_fahrenheit = convert_to_celcius(fahrenheit);
+
+    let converted_celcius = convert_to_fahrenheit(converted_fahrenheit);
+
+    println!("we start with {}F, which converts to {}c, and back to {}F", fahrenheit, converted_fahrenheit, converted_celcius);
+}
+
+fn convert_to_celcius(fahrenheit: f32) -> i32 {
+    let celcius = ((fahrenheit - 32.0) * 5.0) / 9.0;
+    celcius as i32
+}
+
+fn convert_to_fahrenheit(celcius: i32) -> f32 {
+    let doubled_celc = (celcius * 2) as f32;
+    let fahrenheit = (doubled_celc - doubled_celc * 0.1) + 32 as f32;
+    fahrenheit
 }
