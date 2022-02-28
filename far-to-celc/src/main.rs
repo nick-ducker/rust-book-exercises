14a50b0dd21881e96c1af123c613eb28e368031e
Nick Ducker <nick_ducker@live.com>
2022-03-01 00:15:41 +1100

    add basic interaction loop to calculator

--- far-to-celc/src/main.rs
+++ far-to-celc/src/main.rs
@@ -1,4 +1,50 @@
+use std::io;
+
 fn main() {
+    let mut running = true;
+    while running {
+        let mut measurement = String::new();
+
+        println!("Are we converting to C or F?");
+        io::stdin().read_line(&mut measurement)
+            .expect("Error reading input");
+        let measurement = measurement.trim();
+
+        if measurement == "C" {
+            let mut celcius = String::new();
+            println!("Input the degrees celcius to convert");
+            io::stdin().read_line(&mut celcius)
+                .expect("Error reading input");
+
+            let celcius = celcius
+                .trim()
+                .parse()
+                .expect("Could not parse value into integer");
+
+            let result =  convert_to_fahrenheit(celcius);
+
+            println!("This converts to: {}F", result);
+
+        } else if measurement == "F" {
+            let mut fahrenheit = String::new();
+            println!("Input the fahreneight to convert");
+            io::stdin().read_line(&mut fahrenheit)
+                .expect("Error reading input");
+            let fahrenheit: i32 = fahrenheit
+                .trim()
+                .parse()
+                .expect("Could not parse into an integer");
+
+            let result =  convert_to_celcius(fahrenheit as f32);
+
+            println!("This converts to: {}c", result);
+        } else {
+            println!("Thats not C or F");
+            continue;
+        }
+
+        running = false;
+    }
     let fahrenheit = 104.00;
 
     let converted_fahrenheit = convert_to_celcius(fahrenheit);
