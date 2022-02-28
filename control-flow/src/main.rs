2f73a3ac1f1465e682d10d7d07c62f77cf3c15f9
Nick Ducker <nick_ducker@live.com>
2022-02-28 23:01:44 +1100

    add basic control flow example

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 9000;
+
+    if number < 9001 {
+        println!("This is a boring number")
+    } else {
+        println!("IT'S OVER 9000!")
+    }
 }
