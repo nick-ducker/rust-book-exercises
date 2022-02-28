18e5573ce8476a8b27457d033b15a843665225c8
Nick Ducker <nick_ducker@live.com>
2022-02-28 22:38:08 +1100

    add new function with params

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function_boi(5);
+}
+
+fn another_function_boi(param: i32) {
+    println!("Oh, hello, its a new function!");
+
+    println!("You gave me the param of {}", param)
 }
