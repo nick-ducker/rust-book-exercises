9775951389575bb78c92aa5b0dd4992ec97096b5
Nick Ducker <nick_ducker@live.com>
2022-02-28 22:43:17 +1100

    add new 2 parameter function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     println!("Hello, world!");
 
     another_function_boi(5);
+
+    two_param_boi(6, 'k');
+}
+
+fn two_param_boi(x: i32, unit: char) {
+    println!("The measurement is {}{}, I think",x ,unit)
 }
 
 fn another_function_boi(param: i32) {
