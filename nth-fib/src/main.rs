82db931f49430be1c8ffef032cd20554f03e7ab2
Nick Ducker <nick_ducker@live.com>
2022-03-01 22:37:55 +1100

    add first input

--- nth-fib/src/main.rs
+++ nth-fib/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut running = true;
+    
+    while running {
+        let mut nth = String::new();
+    
+        println!("Enter the number of fibz you want");
+        io::stdin().read_line(&mut nth)
+            .expect("failed to read input");
+        let nth: String = match nth.trim().parse() { 
+            Ok(num) => num,
+            Err(_) => {
+                println!("Sorry m8, not a number");
+                continue;
+            },
+        };
+        
+        println!("you entered {}", nth);
+        running = false;
+    }
 }
