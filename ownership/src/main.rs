491ee96008fec54115e18c233a37f077ce1074c0
Nick Ducker <nick_ducker@live.com>
2022-03-05 21:02:22 +1100

    add normal string type

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    // This is a string literal,
+    // This is a hardcoded string literal,
     // This is stored on the stack within the binary =D
+    // It is immutable, which makes life a bit harder
     let string_literal = "Hello there";
+
+    println!("{}", string_literal);
+
+    // This is a String that is *built* from a literal
+    // This can be mutated, yay!
+    let mut string_typed_string = String::from("Hello here");
+    string_typed_string.push_str(" Mr Nick");
+
+    println!("{}", string_typed_string + ", beep");
 }
