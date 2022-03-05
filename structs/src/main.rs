9a434ed7c93c3a6fac5e1508857bd368bb220220
Nick Ducker <nick_ducker@live.com>
2022-03-05 22:53:20 +1100

    add simple structd with notes

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,36 @@
+// Object-esque structs
+struct Climber {
+    active: bool,
+    name: String,
+    ewbank: usize,
+}
+
+// Tuple structs
+struct Coords (usize, usize);
+struct Colour(usize, usize, usize);
+
 fn main() {
-    println!("Hello, world!");
+    let climber_1 = build_climber(String::from("Blueys Bob"), 29);
+
+    let similar_climber = Climber {
+        name: String::from("Nowra Nelson"), 
+        ..climber_1
+    };
+
+    // At this point we can still use climber_1 as we have not moved
+    // its string value to similar_climber. The bool and usize values
+    // are copied, as they live on the stack not the heap.
+    // Had we taken the name of climber_1, the variable would now be
+    // unusable.
+
+    
+
+}
+
+fn build_climber(name: String, ewbank: usize) -> Climber {
+    Climber {
+        name,
+        ewbank,
+        active: true,
+    }
 }
