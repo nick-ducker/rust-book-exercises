11a9271b52280a37bad2587eeb81ecbeba6dc7f0
Nick Ducker <nick_ducker@live.com>
2022-03-08 22:16:38 +1100

    add width function

--- struct-program/src/main.rs
+++ struct-program/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -18,10 +22,13 @@ fn main() {
     };
     
     dbg!(&rect1);
-    println!(
-        "The area of the rectangle is {} square pixels", 
-        rect1.area()
-    );
+
+    if rect1.width() {
+        println!(
+            "The area of the rectangle is {} square pixels", 
+            rect1.area()
+        );
+    }
 }
 
 // fn area(rectangle: &Rectangle) -> u32 {
